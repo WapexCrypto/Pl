038f334c1a32d22465fd01758d5837ac19f8a972
WapexCrypto <114419973+WapexCrypto@users.noreply.github.com>
2022-09-27 12:51:25 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Welcome from "./Welcome";
 import About from "./About";
 import Skills from "./Skills";
