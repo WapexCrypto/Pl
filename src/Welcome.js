c9c21eff112e1bb84c1b187e9a887384dbf40859
WapexCrypto <114419973+WapexCrypto@users.noreply.github.com>
2022-09-28 15:02:15 -0400

    Update Welcome.js

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,10 +1,28 @@
 import React from "react";
+const axios = require('axios');
 
-const Welcome = () => (
-  <div>
-    <h2> Name </h2>
+function xlm() {
+  axios.get('https://api.coingecko.com/api/v3/simple/price?ids=stellar&vs_currencies=eur&include_24hr_change=true')
+  .then(res => {
+    return('€' + String(res.data.stellar.eur).slice(0, 6) + ' (' + String(res.data.stellar.eur_24h_change).slice(0, 5) + '% 24H)');
+  })
+  .catch(error => {
+    console.error(error);
+  });
+function Welcome = () => (
+  function xlm() {
+  axios.get('https://api.coingecko.com/api/v3/simple/price?ids=stellar&vs_currencies=eur&include_24hr_change=true')
+  .then(res => {
+    return('€' + String(res.data.stellar.eur).slice(0, 6) + ' (' + String(res.data.stellar.eur_24h_change).slice(0, 5) + '% 24H)');
+  })
+  .catch(error => {
+    console.error(error);
+  });
+  return ( <div>
+    <h2> { xlm() } </h2>
     Welcome To my Page
   </div>
+)
 );
 
 export default Welcome;
