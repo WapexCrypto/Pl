c6bbf64ae3f5fd7b9f60849de47d36d21ce735b8
WapexCrypto <114419973+WapexCrypto@users.noreply.github.com>
2022-09-30 11:04:36 -0400

    Update Welcome.js

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,14 +1,5 @@
 import React from "react";
 const axios = require('axios');
-
-function xlm() {
-  axios.get('https://api.coingecko.com/api/v3/simple/price?ids=stellar&vs_currencies=eur&include_24hr_change=true')
-  .then(res => {
-    return('€' + String(res.data.stellar.eur).slice(0, 6) + ' (' + String(res.data.stellar.eur_24h_change).slice(0, 5) + '% 24H)');
-  })
-  .catch(error => {
-    console.error(error);
-  });
 function Welcome = () => (
   function xlm() {
   axios.get('https://api.coingecko.com/api/v3/simple/price?ids=stellar&vs_currencies=eur&include_24hr_change=true')
